38d4a610011ef5ee50f77df63c6cada75a755017
Bharat Bhushan <rinku.k3@gmail.com>
2024-11-28 22:15:23 -0500

    Delete AccountMidtierIntf directory

--- AccountMidtierIntf/src/main/java/com/td/mbna/msec/midtier/account/payload/getrewardsbalances/GetRewardsBalancesRequest.java
+++
@@ -1,8 +0,0 @@
-package com.td.mbna.msec.midtier.account.payload.getrewardsbalances;
-
-import com.td.mbna.msec.midtier.types.common.BaseRequest;
-
-public class GetRewardsBalancesRequest extends BaseRequest{
-	private static final long serialVersionUID = 1539875065469997548L;
-	
-}
--- AccountMidtierIntf/src/main/java/com/td/mbna/msec/midtier/account/payload/retrieveipplanlist/RetrieveIpPlanListRequest.java
+++
@@ -1,9 +0,0 @@
-package com.td.mbna.msec.midtier.account.payload.retrieveipplanlist;
-
-import com.td.mbna.msec.midtier.types.common.BaseRequest;
-
-public class RetrieveIpPlanListRequest extends BaseRequest{
-
-	private static final long serialVersionUID = -1569184844734858075L;
-
-}
