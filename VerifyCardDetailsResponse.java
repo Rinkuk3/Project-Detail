ffebd0d2829f3af1dca78777f050add92e70f8d4
Bharat Bhushan <rinku.k3@gmail.com>
2024-11-28 23:00:06 -0500

    Delete VerifyCardDetailsResponse.java

--- VerifyCardDetailsResponse.java
+++
@@ -1,9 +0,0 @@
-package com.td.mbna.msec.midtier.account.payload.addaccount;
-
-import lombok.Data;
-
-@Data
-public class VerifyCardDetailsResponse {
-    private Boolean success;
-    private String errorCode;
-}
