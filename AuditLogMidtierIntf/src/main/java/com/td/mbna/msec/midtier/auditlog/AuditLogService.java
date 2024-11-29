60d7d71e869240b7797944916f3895a8bd83f4d1
Bharat Bhushan <rinku.k3@gmail.com>
2024-11-28 22:15:37 -0500

    Delete AuditLogMidtierIntf directory

--- AuditLogMidtierIntf/src/main/java/com/td/mbna/msec/midtier/auditlog/AuditLogService.java
+++
@@ -1,9 +0,0 @@
-package com.td.mbna.msec.midtier.auditlog;
-
-import com.td.mbna.msec.midtier.auditlog.payload.request.CreateAuditLogRequest;
-import com.td.mbna.msec.midtier.auditlog.payload.response.CreateAuditLogResponse;
-
-public interface AuditLogService {
-	public CreateAuditLogResponse createAuditLogRequest(CreateAuditLogRequest request);
-
-}
