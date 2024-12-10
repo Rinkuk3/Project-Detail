91af031a7e980e6ed8ab15363e8f9670d7625f96
Bharat Bhushan <rinku.k3@gmail.com>
2024-12-10 13:02:37 -0500

    Delete customerprofile directory

--- customerprofile/payload/customeragreement/CaptureAgreementResponse.java
+++
@@ -1,6 +0,0 @@
-package com.td.mbna.msec.midtier.customerprofile.payload.customeragreement;
-
-import com.td.mbna.msec.midtier.types.common.BaseResponse;
-
-public class CaptureAgreementResponse extends BaseResponse {
-}
