5c0531e1e34f30a501e15f6a9be43bd44fc4652a
Bharat Bhushan <rinku.k3@gmail.com>
2025-02-20 13:09:20 -0500

    Add files via upload

---
+++ updateofferdisposition/PartyResponse.java
@@ -0,0 +1,50 @@
+package com.td.api.mbofp.response.updateofferdisposition;
+
+public class PartyResponse {
+
+    private String eventId;
+
+    public String getEventId() {
+        return eventId;
+    }
+
+    public void setEventId(String eventId) {
+        this.eventId = eventId;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append(PartyResponse.class.getName()).append('@').append(Integer.toHexString(System.identityHashCode(this))).append('[');
+        sb.append("eventId");
+        sb.append('=');
+        sb.append(((this.eventId == null)?"<null>":this.eventId));
+        sb.append(',');
+        if (sb.charAt((sb.length()- 1)) == ',') {
+            sb.setCharAt((sb.length()- 1), ']');
+        } else {
+            sb.append(']');
+        }
+        return sb.toString();
+    }
+
+    @Override
+    public int hashCode() {
+        int result = 1;
+        result = ((result* 31)+((this.eventId == null)? 0 :this.eventId.hashCode()));
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        if (other == this) {
+            return true;
+        }
+        if ((other instanceof PartyResponse) == false) {
+            return false;
+        }
+        PartyResponse rhs = ((PartyResponse) other);
+        return ((this.eventId == rhs.eventId)||((this.eventId!= null)&&this.eventId.equals(rhs.eventId)));
+    }
+
+}
