768f3667995eb0cce28bcc7ecbe5961a14281e3f
Bharat Bhushan <rinku.k3@gmail.com>
2025-03-04 01:35:05 -0500

    Delete service directory

--- service/MsecValidator.java
+++
@@ -1,7 +0,0 @@
-package com.td.mbna.msec.midtier.service;
-
-import com.td.mbna.msec.midtier.types.common.ValidationException;
-
-public interface MsecValidator {
-    public ValidationException validate(Object request);
-}
